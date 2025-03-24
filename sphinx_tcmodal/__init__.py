b8d23236f2efa10098df9928e9ad9acdfac799db
Konrad Weihmann <konrad.weihmann@avnet.eu>
2025-03-24 11:29:09 +0000

    tcmodal: fix relative image paths
    instead of using the original file path, we will need to
    wait till the entire doctree is created, as e.g. when tcmodal
    is used from an included file, the location info is wrong.
    
    So we create just a template string and replace that one
    in a second run
    
    Signed-off-by: Konrad Weihmann <konrad.weihmann@avnet.eu>

--- sphinx_tcmodal/__init__.py
+++ sphinx_tcmodal/__init__.py
@@ -6,7 +6,7 @@ import os
 import shutil
 import json
 
-from docutils.nodes import Node, raw
+from docutils.nodes import Node, raw, document
 from docutils.parsers.rst import directives
 from sphinx.application import Sphinx
 from sphinx.locale import _, __
@@ -22,6 +22,11 @@ module_dir = os.path.dirname(__file__)
 assets_dir = os.path.join(module_dir, 'assets')
 
 _tcmodal_counter = 0
+
+
+class tcmodal_raw(raw):
+    """tcmodal raw node"""
+
 
 class TermsAndConditionsModal(SphinxDirective):
     """
@@ -53,10 +58,8 @@ class TermsAndConditionsModal(SphinxDirective):
                     <label for="checkbox-{_id}"><a href="{link}" target="_blank">I read and I agree to {name}</a></label>
                 <div>
             '''
-        img_path = os.path.join(
-            self._docrelpath, '_images', os.path.basename(self._image))
         result = f'''
-        <img width="{self._width}" height="{self._height}" src="{img_path}" alt="{self._alttext}" onclick="tcmodal(\'{self._id}\')">
+        <img width="{self._width}" height="{self._height}" src="%RELPATH%/_images/{os.path.basename(self._image)}" alt="{self._alttext}" onclick="tcmodal(\'{self._id}\')">
         <div id="{self._id}" class="tcmodal">
             <div class="tcmodal-content">
                 <strong>
@@ -79,7 +82,6 @@ class TermsAndConditionsModal(SphinxDirective):
 
         _tcmodal_counter += 1
         file, line = self.get_source_info()
-        self._docrelpath = os.path.relpath(env.tcmodal_root_path, os.path.dirname(file))
 
         loc_info = f'{file}-{line}-{_tcmodal_counter}'
         self._id = sha1(loc_info.encode()).hexdigest()
@@ -97,11 +99,15 @@ class TermsAndConditionsModal(SphinxDirective):
         if not self._image:
             raise Exception('image is missing')
 
-        fullpath = os.path.join(os.path.dirname(file), self._image)
-        env.tcmodal_image_files[fullpath] = fullpath
-        self.env.app.emit('env-updated', env)
+        fullpath = os.path.abspath(os.path.join(
+            os.path.dirname(file), self._image))
+        if os.path.isfile(fullpath):
+            env.tcmodal_image_files[fullpath] = fullpath
+            self.env.app.emit('env-updated', env)
         rawsource = self._create_raw()
-        return [raw('', rawsource, format='html')]
+        new_node = tcmodal_raw('', rawsource, format='html')
+        self.set_source_info(new_node)
+        return [new_node]
 
 
 def copy_asset_files(app: Sphinx, exc):
@@ -109,6 +115,7 @@ def copy_asset_files(app: Sphinx, exc):
         static_dir = os.path.join(app.builder.outdir, '_static')
         for file in glob.glob(assets_dir + '/*'):
             copy_asset(file, static_dir, force=True)
+
 
 def install_static_files(app, env):
     images_dir = os.path.join(app.builder.outdir, '_images')
@@ -124,14 +131,47 @@ def install_static_files(app, env):
             os.remove(_targetpath)
         shutil.copy(image, _targetpath)
 
+
 def builder_init_hook(app: Sphinx):
-    app.env.tcmodal_root_path = app.builder.srcdir
     app.env.tcmodal_image_files = {}
-                                
+
+
+def process_tree(app: Sphinx, doctree: document, fromdocname: str):
+
+    def replace_relpath(node, relpath):
+        if not isinstance(node, tcmodal_raw):
+            return
+        new_content = node.rawsource.replace('%RELPATH%', relpath)
+        node.replace_self([tcmodal_raw('', new_content, format='html')])
+
+    for node in doctree.findall(tcmodal_raw, ascend=True):
+        node: tcmodal_raw = node
+        try:
+            _source = node.source
+            _, ext = os.path.splitext(_source)
+            if ext not in ['.rst', '.md']:
+                _source = node.parent.source
+            # logger.info(f'parent: {node.parent.source}')
+            # logger.info(f'source: {node.source} -> {_source}')
+            # logger.info(
+            #     f'children: {node.children} -- {[node.source for x in node.children]}')
+            relpath = os.path.relpath(
+                app.builder.srcdir, os.path.dirname(_source))
+            if relpath == './':
+                relpath = ''
+
+            for child in node.children:
+                replace_relpath(child, relpath)
+            replace_relpath(node, relpath)
+        except Exception as e:
+            pass
+
+
 def setup(app: Sphinx) -> ExtensionMetadata:
     app.connect('build-finished', copy_asset_files)
     app.connect('builder-inited', builder_init_hook)
     app.connect('env-updated', install_static_files)
+    app.connect('doctree-resolved', process_tree)
 
     directives.register_directive('tcmodal', TermsAndConditionsModal)
 
