8b805ccc44d7e96ea011b95f8bf65c6895a3e646
Konrad Weihmann <konrad.weihmann@avnet.eu>
2025-04-08 06:22:05 +0000

    userjourney: add new directive
    Signed-off-by: Konrad Weihmann <konrad.weihmann@avnet.eu>

--- setup.py
+++ setup.py
@@ -14,6 +14,12 @@ setup(
              'sphinx_tcmodal/assets/tcmodal.js',
              'sphinx_tcmodal/assets/tcmodal.css',
         ]),
+        ('sphinx_userjourney_bar',
+         [
+             'sphinx_userjourney_bar/assets/userjourney.css',
+             'sphinx_userjourney_bar/assets/_images/userjourney-step.png',
+             'sphinx_userjourney_bar/assets/_images/userjourney-sub.png',
+        ]),
     ],
     include_package_data=True,
     install_requires = install_requires,
---
+++ sphinx_userjourney_bar/__init__.py
@@ -0,0 +1,210 @@
+from __future__ import annotations
+
+import glob
+import os
+import json
+import shutil
+
+from docutils.nodes import Node, raw
+from docutils.parsers.rst import directives
+from sphinx.application import Sphinx
+from sphinx.locale import _, __
+from sphinx.util import logging
+from sphinx.util.docutils import SphinxDirective
+from sphinx.util.typing import ExtensionMetadata
+
+logger = logging.getLogger(__name__)
+
+module_dir = os.path.dirname(__file__)
+assets_dir = os.path.join(module_dir, 'assets')
+
+
+class UserJourney(SphinxDirective):
+    """
+    Directive creating user journey breadcrumb like bar.
+    """
+    has_content = False
+    required_arguments = 0
+    optional_arguments = 0
+    final_argument_whitespace = True
+
+    option_spec = {
+        "active-section": directives.unchanged,
+        "active-subsection": directives.unchanged,
+        "active-step": directives.unchanged,
+        "sections": directives.unchanged,
+        "subsections": directives.unchanged,
+        "steps": directives.unchanged,
+    }
+
+    def _create_raw(self) -> str:
+
+        def render_ul(list_: list, active, def_css, act_css, top_css, pre_list=None):
+            result = f'<ul class="{top_css}"">\n'
+            if pre_list:
+                result += pre_list
+
+            prefix = ''
+            suffix = ''
+
+            list_len = len(list_)
+
+            if list_len > 3:
+                index = list_.index(active) + 1
+                index_from = max(0, index - 1)
+                index_to = min(index + 1, list_len)
+                iterlist = list_[index_from:index_to]
+                if index_from != 0:
+                    prefix = f'<li><span class="{def_css}">...</span></li>\n'
+                if index_to != list_len:
+                    suffix = f'<li><span class="{def_css}">...</span></li>\n'
+            else:
+                iterlist = list_
+            result += prefix
+            for item in iterlist:
+                if item == active:
+                    result += f'<li><span class="{act_css}">{item}</span></li>\n'
+                else:
+                    result += f'<li><span class="{def_css}">{item}</span></li>\n'
+            result += suffix
+            result += "</ul>\n"
+            return result
+
+        result = '<div class="before-userjourney"></div>'
+        result += render_ul(list(self._sections.keys()), self._active_section,
+                            'userjourney-level-top', 'userjourney-level-top-active', 'userjourney')
+
+        if self._has_subsection:
+            result += render_ul(self._subsections, self._active_subsection,
+                                'userjourney-level-sub',
+                                'userjourney-level-sub-active',
+                                'userjourney userjourney-subsection',
+                                pre_list='<img class="pre-userjourney-subsection"/>'
+                                )
+
+            if self._has_step:
+                result += render_ul(self._steps, self._active_step, 'userjourney-level-step',
+                                    'userjourney-level-step-active',
+                                    'userjourney userjourney-step',
+                                    pre_list='<img class="pre-userjourney-step"/>')
+
+        return result
+
+    def _parse_sections(self):
+        self._active_section = self.options.get('active-section')
+        try:
+            self._sections = json.loads(self.options.get(
+                'sections', '{}')) or self.env.app.config.userjourney_sections
+            if not self._active_section:
+                self.error('active-section needs to be set')
+            if not self._active_section in self._sections.keys():
+                self.error('active-section is not known')
+        except json.JSONDecodeError:
+            self.error('sections is not a dictionary')
+
+    def _parse_subsections(self):
+        self._active_subsection = self.options.get('active-subsection', '')
+        self._has_subsection = len(self._active_subsection) > 0
+        if not self._has_subsection:
+            return
+        if any(self.options.get('subsections', '').split(',')):
+            self._subsections = [x.strip() for x in self.options.get(
+                'subsections', '').split(',')]
+        else:
+            self._subsections = [x.strip()
+                                 for x in self._sections.get(self._active_section)]
+        if not self._active_subsection in self._subsections:
+            self.error('active-subsection is not known')
+
+    def _parse_steps(self):
+        self._active_step = self.options.get('active-step', '')
+        self._has_step = len(self._active_step) > 0
+        if not self._has_step:
+            return
+        self._steps = [x.strip()
+                       for x in self.options.get('steps', '').split(',')]
+        if not self._active_step in self._steps:
+            self.error('active-step is not known')
+
+    def run(self) -> list[Node]:
+        self._parse_sections()
+        self._parse_subsections()
+        self._parse_steps()
+        return [raw('', self._create_raw(), format='html')]
+
+
+def copy_asset_files(app: Sphinx, exc):
+    def patch_css(file):
+        with open(file, 'r+') as i:
+            content = i.read()
+
+            for item in ['userjourney_border_color',
+                         'userjourney_section_bg_color',
+                         'userjourney_section_bg_color_active',
+                         'userjourney_section_txt_color',
+                         'userjourney_section_txt_color_active',
+                         'userjourney_subsection_bg_color',
+                         'userjourney_subsection_bg_color_active',
+                         'userjourney_subsection_txt_color',
+                         'userjourney_subsection_txt_color_active',
+                         'userjourney_step_bg_color',
+                         'userjourney_step_bg_color_active',
+                         'userjourney_step_txt_color',
+                         'userjourney_step_txt_color_active']:
+                val = getattr(app.config, item, None)
+                if val is None:
+                    continue
+                content = content.replace(f'%{item}%', str(val))
+
+            i.seek(0)
+            i.truncate()
+
+            i.write(content)
+
+    if app.builder.format == 'html' and not exc:
+        static_dir = os.path.join(app.builder.outdir, '_static')
+        for file in glob.glob(assets_dir + '/*') + glob.glob(assets_dir + '/**/*'):
+            if not os.path.isfile(file):
+                continue
+            relpath = os.path.relpath(file, assets_dir)
+            targetpath = os.path.join(static_dir, relpath)
+            os.makedirs(os.path.dirname(targetpath), exist_ok=True)
+            shutil.copy(file, targetpath)
+        patch_css(os.path.join(static_dir, 'userjourney.css'))
+
+
+def setup(app: Sphinx) -> ExtensionMetadata:
+    app.connect('build-finished', copy_asset_files)
+
+    app.add_config_value('userjourney_sections', {}, False)
+
+    app.add_config_value('userjourney_border_color', 'white', 'html')
+    app.add_config_value('userjourney_section_bg_color',
+                         'var(--sd-color-primary)', 'html')
+    app.add_config_value('userjourney_section_bg_color_active',
+                         'var(--sd-color-primary-highlight)', 'html')
+    app.add_config_value('userjourney_section_txt_color', 'white', 'html')
+    app.add_config_value(
+        'userjourney_section_txt_color_active', 'white', 'html')
+    app.add_config_value('userjourney_subsection_bg_color',
+                         'var(--sd-color-secondary)', 'html')
+    app.add_config_value('userjourney_subsection_bg_color_active',
+                         'var(--sd-color-secondary-highlight)', 'html')
+    app.add_config_value('userjourney_subsection_txt_color', 'white', 'html')
+    app.add_config_value(
+        'userjourney_subsection_txt_color_active', 'white', 'html')
+    app.add_config_value('userjourney_step_bg_color',
+                         'var(--sd-color-muted)', 'html')
+    app.add_config_value('userjourney_step_bg_color_active',
+                         'var(--sd-color-info-highlight)', 'html')
+    app.add_config_value('userjourney_step_txt_color', 'white', 'html')
+    app.add_config_value('userjourney_step_txt_color_active', 'white', 'html')
+
+    directives.register_directive('userjourney', UserJourney)
+
+    app.add_css_file('userjourney.css')
+
+    return {
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+    }
